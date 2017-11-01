f82fee688fb8941506e7c4a3042bb1efd265f392
Milos Arbanas <milos.arbanas@frobas.com>
2017-11-01 16:50:51 +0100

    Initial commit.

---
+++ genetic_algorithm/f1_bnlp.m
@@ -0,0 +1,11 @@
+function y = f1_bnlp(in)
+x1 = in(1);
+x2 = in(2);
+x3 = in(3);
+x4 = in(4);
+x5 = in(5);
+x6 = in(6);
+
+
+
+y = 1 - (x1+x2+x3)*(2-x2+x4-x5)*(3-x3-x5-x6);
---
+++ genetic_algorithm/f2_bnlp.m
@@ -0,0 +1,13 @@
+function y = f2_bnlp(in)
+x1 = in(1);
+x2 = in(2);
+x3 = in(3);
+x4 = in(4);
+x5 = in(5);
+x6 = in(6);
+x7 = in(7);
+x8 = in(8);
+
+
+
+y = 1 - f1_bnlp(in([1:6]))*(2-x4+x6-x7)*(1-x1+x3+x5)*(3-x6-x7-x8)
